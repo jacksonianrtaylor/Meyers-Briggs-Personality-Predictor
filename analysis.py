891c34a4620667afd6ee3bf9c5d66eb93b39fece
jacksonianrtaylor <jacksonian.r.taylor@gmail.com>
2023-07-30 18:07:07 -0700

    progress with inline comments

--- analysis.py
+++ analysis.py
@@ -90,27 +90,22 @@ def dec_tree_model(X_train,X_test,y_train,y_test):
     return scores[0]    
     
 
-#test class
-#this is what is used to test the accruacy of models for the number of featurs inputs...
-#that are selelcted by chi-squared
-#remebr teh inputs are simple word counts
+#tests class
+#this is what is used to test the accuracy of models for varied number of feature inputs...
+#that are selelcted with chi-squared feature selection
+#the inputs are simply word counts
 class tests():   
     def __init__(self):
-        #This is used for consistent train test splits for a given runtime...
-        #for all models
+        #This is used for consistent train test splits for a given runtime for all models
         #however, it has been replaced with a constant seed...
-        #to produce simlair test train splits as well as consistencey over runtimes
+        #to produce simlair consistencey over runtimes
+        #see rseed variable
         self.seed = random.randint(0, 42) 
 
     def preprocess(self, i,X, y):
-
-        #note input must be non-negative to use chi2 feature selection
-        #should test train split come before select k best???
-
         #note: the current configuration uses only the train data to fit the selectKbest selector
         #This is to simulate a simple case of testing the model with new data
-        #the test data can use the selector but should not contribute to fiting it
-
+        #the test data can use the "selector" but should not contribute to fiting it
         X_train,X_test,y_train,y_test = train_test_split(X,  y,test_size = 50, random_state = rseed)
 
         selector = SelectKBest(chi2, k=i)
@@ -120,7 +115,7 @@ class tests():
 
         return X_train,X_test,y_train,y_test
 
-    def test_features(self,i, model_id,X, y ):
+    def test_features(self,i, model_id, X, y ):
         #https://blog.finxter.com/python-list-copy/
         #copy is atomic and rest of the variables are thread safe in the current namespace
         #this also re-intitializes the data in the local scope
@@ -129,10 +124,11 @@ class tests():
         y = y.copy()    
         X_train,X_test,y_train,y_test = self.preprocess(i,X, y)   
 
-        #model selection
+        #model selector
         #the negation is returned because the optmizer that uses this function...
         #tries to find the minimum by default.
-        #the negation of the minimum becomes the maximum
+        #The optmizers outputs results are negative accuracy scores that can be
+        #negated again to find real accuracy scores
 
         if("dec_tree_model" == model_id):
             return -dec_tree_model(X_train,X_test,y_train,y_test)
@@ -145,42 +141,44 @@ class tests():
         
 
 #main
-#load data from tf_matrix.csv
+#load data from tf_matrix.csv that was populated by the transform program
 time_t = time.time()
 f = open('tf_matrix.csv', 'r', encoding="utf-8")
 data = pd.read_csv(f, header=0) 
 
 #create pairs list
+#used for outputs
 pairs  =  ["_I_E_","_N_S_", "_T_F_", "_J_P_"] 
 
-#features are all word/tokens the occur in at least one text
+#features are all word/tokens that occur in at least one text
 features = list(data.keys())
+#omit all but the words
 features = features[:-5]
 
-#X is all the occurances of the words for each text for each user if data frame form
+#X is all the occurances of the words for each user
 X = data[features]
 
-# convert data frame to numpy array
-# This does not seem to be necessary
-# X = X.values
+# convert data frame to numpy array with just counts
+# This may not be necessary...
+X = X.values
 
 #convert to csr matrix becuase the numpy array is sparse
 X = csr_matrix(X)
 
-#instantiate an object
 classification_tests = tests()
-#the best model for predicting each pair
+
+#the most accurate model for predicting each pair
 Best_in_class = []
 
-#used to output to the rsults file
+#used to output to the results file
 out_df = pd.DataFrame()
  
 
-print ("Testing 39 users for each of the 16 meyers briggs personalities...\n")
-c  =0
+print("Testing 39 users for each of the 16 meyers briggs personalities...\n")
+c = 0
 for item in pairs:  
     c = c+1
-    #not necessary to use list here...
+    #not necessary to use list(data[item]) here...
     y = data[item]
     #each model is tested for each pair prediction
     local_optimas_dec  = []
@@ -193,9 +191,13 @@ for item in pairs:
     print(item, "Classification:")
     while(i<6):           
         
-        #uses multithreading for small to medium improvements in runtime   
-        #the negation is used because the above funtions finds the fminbound for accuracy
-        # and the opposite is the maximum accuracy 
+        #uses multithreading for small to medium improvements in runtime  
+
+        #the negation is returned from test_features because the optmizer that uses this function...
+        #tries to find the minimum by default.
+        #The optmizers outputs results includes accuracy scores that can be...
+        #negated again to find real accuracy scores
+
         t1 = return_thread(group=None,target=fminbound,
                            kwargs={"func" : classification_tests.test_features,"x1" : (i)*gap_size+1, "x2" : (i+1)*gap_size+1,"args": ("dec_tree_model",X, y), "full_output" : True, "disp" :0})
         t2 = return_thread(group=None,target=fminbound,
@@ -220,7 +222,7 @@ for item in pairs:
         # local_optima_3 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("rand_forest_model",X, y), full_output  = True, disp   =0)
         # local_optima_4 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("naive_bays_model",X, y), full_output  = True, disp   =0)
         
-        #the negation is used because the above funtions finds the fminbound and we want the opposite
+        #the negation is returned (see above)
         local_optimas_dec.append((-local_optima_1[1], int(local_optima_1[0])))
         local_optimas_reg.append((-local_optima_2[1], int(local_optima_2[0])))
         local_optimas_for.append((-local_optima_3[1], int(local_optima_3[0])))
@@ -228,32 +230,34 @@ for item in pairs:
 
 
         #local_optima_1[1] is a score
-        #and local_optima_1[0] is number of features used that gave the optimal score 
+        #and local_optima_1[0] is number of features used that gave the accuracy score 
+        #this is to show progress but may be out of order with use of multithreading
+
         print("Iteration:", i+1)
         print("Decision Tree Test:",  "Score:",-local_optima_1[1], "Features:", int(local_optima_1[0]))                       
         print("Logistic Regression Test:",  "Score:",-local_optima_2[1], "Features:", int(local_optima_2[0]))        
         print("Random Forest Test:", "Score:",-local_optima_3[1], "Features:", int(local_optima_3[0]))       
         print("Naive Bays Test:",  "Score:",-local_optima_4[1], "Features:", int(local_optima_4[0]),"\n")   
         
-        #i controls the number of features
+        #i controls the number of features to optimize in the model
         #the optimizer finds the best number of features within bounds (i)*gap_size+1 and (i+1)*gap_size+1
         #then iterates again with i = i+1
         i += 1
 
-    #find the best number features for each model for a certain personality pair
-    #check to see how pairs/tuples are sorted...
+    #find the best accuracy for the best number of features for each model for a certain personality pair
     local_optimas_dec.sort(reverse  = True) 
     local_optimas_reg.sort(reverse  = True)
     local_optimas_for.sort(reverse  = True)
     local_optimas_bays.sort(reverse  = True)
     
+    #outputs to be used later in results.csv
     out_df[item] = ["Decision Tree: "+"Features: "+str(local_optimas_dec[0][1])+" Accuracy: "+str(local_optimas_dec[0][0])
     ,"Logistic Regession: "+"Features: "+str(local_optimas_reg[0][1])+" Accuracy: "+str(local_optimas_reg[0][0])
     ,"Random Forest: "+ "Features: "+str(local_optimas_for[0][1])+" Accuracy: "+str(local_optimas_for[0][0])
     ,"Naive Bays: "+ "Features: "+str(local_optimas_bays[0][1])+" Accuracy: "+str(local_optimas_bays[0][0])]
     
 
-    #print all the results of the optimized models
+    #print above results directly above
     print(item, "Classification:")
     print("Best Predictor Function Scores:")
     print("Decision Tree:","Features:",local_optimas_dec[0][1],"Accuracy:",local_optimas_dec[0][0])
@@ -262,8 +266,8 @@ for item in pairs:
     print("Naive Bays:", "Features:",local_optimas_bays[0][1],"Accuracy:",local_optimas_bays[0][0],"\n")
     
     #Best of class is a list of optimal scores for each model for each personality pair
-    #note that this append is just for a single personality pair
-    #note local_optimas_dec[0][0] gives the best score since local_optimas_dec is sorted backwards
+    #note: This append is just for a single personality pair
+    #note: local_optimas_dec[0][0] gives the best decision tree score since local_optimas_dec is sorted backwards
     Best_in_class.append([local_optimas_dec[0][0], 
                           local_optimas_reg[0][0], 
                           local_optimas_for[0][0], 
@@ -271,9 +275,13 @@ for item in pairs:
 
 
 
-#this is the main code
+#main
 results = []
+
+#used to print results
 classifiers = ["Decision Tree: ", "Logistic Regression: ", "Random Forest: ", "Naive Bays: "]
+
+#header
 print("Complete Myers Briggs Prediction for individual Classifiers:")
 
 for x in range (0,4):
@@ -282,20 +290,24 @@ for x in range (0,4):
     #item[x] is a score for the xth personailty pair
     for item in Best_in_class:
         product*= item[x]
-    #print the overall score and append to results using law of independence
+    #print the overall score using law of independence and append to results
     print(classifiers[x], product)
     results.append(str(classifiers[x])+ str(product))
+
+#header
 print("\nMyers Briggs Prediction from the best of each Classifier:")
 
 
 #using the law of independence and the best scored model for each personality pair...
-#Note: it is important to state in the readme that one personailty pair prediction does not have an impact on 
-#another personality pair since there is the same number of people for each of the 16 personailties
 #This eliminates bais for common vs uncommon personalities
-#find the absoluted best model
+
+#find the absolute best model
 product  = 1
 for item in Best_in_class:
     item.sort(reverse  = True)
+    #for testing search for "[" ...
+    #test passed
+    print(item)
     product*= item[0]
 
 print(product)
@@ -308,13 +320,16 @@ out_df["Overall Best"] = [str(product), "", "","" ]
 #outputs
 out_file = open('results.csv', 'w', encoding="utf-8")
 out_df.to_csv(out_file,index  = False)
-out_file.close()    
+out_file.close()   
+
+#time to compute
 print("Full compute time:",time.time()-time_t,"Seconds")
 
 
-#Note: it is important to state in the readme that one personailty pair prediction does not have an impact on 
-#another personality pair since there is the same number of people for each of the 16 personailties
+#Note: it is important to state in the readme that one personailty pair prediction should not have an impact on 
+#another personality pair since there is the same number of people for each of the 16 personalilties
 #This eliminates bais for common vs uncommon personalities
+#it means that a full prediction is not made but is theoreticaly accurate based on the predictions of each pair
 
 
 
--- transform.py
+++ transform.py
@@ -21,12 +21,9 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 
-#random seed
-rseed = 5
-random.seed(rseed)
 
-# load and return the data_by_type and data_by_type_full variables populated from the mbti_1.csv
-# data_by_type is a dictionary of personality types to list of users of that personality
+# load and return the data_by_type and data_by_type_full variables populated from mbti_1.csv
+# They are both a dictionary of personality types to list of users of that personality
 # each user is defined by a list of 50 posts that they made
 def load_data():
     f = open('mbti_1.csv', newline='', encoding="utf-8")
@@ -40,7 +37,7 @@ def load_data():
         data_by_type_full[row[0]].append(row[1].split("|||"))
     #Only a randomly selelcted 39 users of each for the 16 personality type are used to train the models
     #That is the number of users for the rarest personality
-    #This way there is no popularity bias built into the model
+    #This way there is no popularity bias built into the model 
     data_by_type = dict()
     for key in data_by_type_full.keys():
         data_by_type[key] = random.sample(data_by_type_full[key], 39)
@@ -49,133 +46,110 @@ def load_data():
 
 
 
-#Ouputs the data to tf_matrix in a format ready for the analysis section
+#Ouputs the data to tf_matrix in a format ready for the analysis program
 def tf_full(pairs, data_by_type):
-    #the word bank is used because all the unqiue terms need to be in a consistent order
+    #the word_bank OrderedSet is used because all the unqiue terms need to be in a consistent order
     word_bank = OrderedSet()
     #count of words occurances for every user
     #the counts themselves are in the order of the word bank
     word_occurances = []
     #this becomes a list of 4 lists of personality bits for each personality pair
     pair_types = [[],[],[],[]]
+    #this is a list of integers corresponding to personality type int (0-15)
     types = []
     #populate word_bank
     for w,x,y,z in itertools.product(pairs[0],pairs[1],pairs[2],pairs[3]):  
         for i in range (0,39): 
             update_word_bank(word_bank ,data_by_type,i, w+x+y+z)           
     c = 0    
-
     #populate word_occurances and pairs
     for w,x,y,z in itertools.product(pairs[0],pairs[1],pairs[2],pairs[3]): 
         for i in range (0,39):   
-            #create a dictionary with the keys the same as the word_bank set
-            #and the values (the # of accorances initially set to 0)
-
-            #how do we know that the order is consistent when word_bank is a set???
-            #does this need to be a dcitionary???
-            #can it also be a list???
-            group_word_occurances = dict.fromkeys(list(word_bank),0)  
-
-            #populate the real count values in group_word_occurances...
+            #init word occurances for a user
+            group_word_occurances = [0] * len(word_bank)
+            #populate group_word_occurances
             update_word_occurances(word_bank, group_word_occurances, i, data_by_type, w+x+y+z) 
-
-            #extract only the occurances without the keys and append to word_occurances
-            #Do the values hold consistent order for each iteration??? (yes)
-            #https://stackoverflow.com/questions/835092/python-dictionary-are-keys-and-values-always-the-same-order#:~:text=Yes%2C%20what%20you%20observed%20is,order%20as%20the%20corresponding%20lists.
-            word_occurances.append(list(group_word_occurances.values()))
-
-            #pairs is a list of the personaility duos
+            #append group_word_occurances for a user to word_occurances
+            word_occurances.append(group_word_occurances)
+            #pairs is a list of the personality pairs with 16 combinations
             #w+x+y+z is a single string representing a single personality
-            #pair_types is a list of booleans to represent each personality pair
-
             #this populates pair_types
-            #add the same value to pair_types the for all 39 sample users of type w+x+y+z
             populate_pairs(pair_types, pairs, w+x+y+z)
-
-            # c is an integer to represent personaility (0-15)
-            # same for all 39 sample users of type w+x+y+z
+            # c is an integer to represent personaility with an integer (0-15)
             types.append(c)    
         # starts at 0 and goes to 15   
         c = c+1        
-    #to_csv          
+    #this is the output of the data prep progam used for the start of the analysis program       
     to_csv("tf_matrix.csv", word_occurances, word_bank, pair_types, types)
     
-    
-def update_word_bank(word_bank,data_by_type,x, key):
-    #look through a single users posts and update the word bank
 
-    #here it is possible to randomly select the users without replacement rather than taking the first 39 users
-    #or the order can simply be scrambled before hand...
+#look through a single users posts and update the word bank 
+def update_word_bank(word_bank,data_by_type,x, key):
     for post in data_by_type[key][x]:
         tokens_1 = word_tokenize(post)
         tokens_2 = [WordNetLemmatizer().lemmatize(token.lower()) for token in tokens_1 if token.isalpha() 
-                    and WordNetLemmatizer().lemmatize(token.lower()) not in stopwords.words('english')]
-        
+                    and WordNetLemmatizer().lemmatize(token.lower()) not in stopwords.words('english')]  
         word_bank.update(tokens_2)
         
-                
-def update_word_occurances(word_bank, group_word_occurances,x, data_by_type,key):       
+#return a list of word counts in the order of the word_bank       
+def update_word_occurances(word_bank, group_word_occurances,x, data_by_type,key): 
+    #all the words that a user used, copies allowed
     bag_of_words = [] 
     #populate bag of words
     for post in data_by_type[key][x]:
-        #seperate all the words and store in tokens
         tokens_1 = word_tokenize(post)
-        #check if the token is in word bank set for each token
         tokens_2 = [WordNetLemmatizer().lemmatize(token.lower()) for token in tokens_1 
                     if WordNetLemmatizer().lemmatize(token.lower()) in word_bank]
         bag_of_words.extend(tokens_2)
-        
-    # go through the word bank set and find each occurance in the bag of words 
-    # and update the value for group_word_occurances
-    # note: order of the word bank does not matter since the group_word_occurances is a dictionary  
 
-    # this might be faster if the bag of words were sorted then counted and put into
-    # group_word_occurances O(n^2) vs O(nlogn) + O(n)
-
+    #populate group_word_occurances in the order of the terms in word_bank set
+    i = 0
     for word in word_bank:
         for instance in bag_of_words:
             if instance == word:
-                group_word_occurances[word] = group_word_occurances[word]+ 1  
+                group_word_occurances[i] = group_word_occurances[i]+ 1 
+        i += 1 
 
-#note: pair_types is a list of booleans that determine the personality combination
+#note: pair_type is a list of 4 lists of personality bits...
+#for the users personailty selection for the given pair
 def populate_pairs(pair_types, pairs, key): 
     c =0
     for item in pairs:
-        if(item[0] in key): 
+        if(item[0] == key[c]): 
             pair_types[c].append(1)
         else:
             pair_types[c].append(0)
         c = c+1
 
-           
+#output to tf_matrix.csv for the analysis program
 def to_csv(file_name, word_occurances, word_bank, pair_types,  types):   
-    #note: word_occurances does not necessarily line up with list(word_bank)
     df = pd.DataFrame(data=word_occurances, columns=list(word_bank))  
     #these columns are the personailty columns 
     df["_I_E_"] = pair_types[0]
     df["_N_S_"] = pair_types[1]
     df["_T_F_"] = pair_types[2]
     df["_J_P_"] = pair_types[3]
-    #this column is the full type column ranging (0-15)
+    #this column is the full type column ranging from int(0-15)
     df["_Type_"] = types
     f = open(file_name, 'w', encoding='utf-8')
     df.to_csv(f,index  = False)
     f.close()    
     
+
+#main program...
 time_t = time.time()
-#note: 
+
+#consistent seed
+rseed = 5
+random.seed(rseed)
+
 data_by_type, data_by_type_full = load_data()                                
 pairs  = [['I', 'E'],['N', 'S'],['T', 'F'],['J', 'P']]
 
-
-print("Data per Personality:")
+print("Orginal data per Personality:")
 for w,x,y,z in itertools.product(pairs[0],pairs[1],pairs[2],pairs[3]):
     print(w+x+y+z, len(data_by_type_full[w+x+y+z]))
     
-
-
 tf_full(pairs, data_by_type)
+print("Done")
 print("Full compute time:",time.time() - time_t,"Seconds")
-
-
-#idea: the 39 users of each personality should be chosen randomly...
