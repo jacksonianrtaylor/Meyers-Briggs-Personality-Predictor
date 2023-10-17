5cb4ab97254b2ec1927e32c9c343352fbad073c0
jacksonianrtaylor <jacksonian.r.taylor@gmail.com>
2023-10-16 19:22:48 -0700

    ouputs are close

--- analysis.py
+++ analysis.py
@@ -18,19 +18,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-#some normalization techniques were tested that decreased the time for model convergence but produced worse results for accurcay scores
-from sklearn.preprocessing import normalize
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-
-
 #global random seed for consistent train test splits 
 #and randomstates for certain models
-rseed = 10
+r_seed = 10
 
 
 class return_thread(Thread):
-    """Thread class that returns a value in the join method"""
+    """Thread class that returns a value with join"""
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs={}, Verbose=None):
         Thread.__init__(self, group, target, name, args, kwargs)
@@ -59,27 +53,28 @@ def log_reg_model(X_train,X_test,y_train,y_test):
     return accuracy_score(y_test, y_pred)
 
 def rand_forest_model(X_train,X_test,y_train,y_test):
-    rand_for = RandomForestClassifier(random_state = rseed)
+    rand_for = RandomForestClassifier(random_state = r_seed)
     rand_for.fit(X_train,y_train)
     y_pred=rand_for.predict(X_test)   
     return accuracy_score(y_test, y_pred)
 
 def dec_tree_model(X_train,X_test,y_train,y_test):
     """The decision tree model implements cost complexity pruning"""
-    dec_tree = DecisionTreeClassifier(random_state = rseed)
+    dec_tree = DecisionTreeClassifier(random_state = r_seed)
     path = dec_tree.cost_complexity_pruning_path(X_train, y_train)
     alphas = path['ccp_alphas']   
-    scores  = []
+    max_score = 0
     for alpha in alphas:
-        scores.append(fit_tree(X_train,X_test,y_train,y_test, alpha))             
-    scores.sort(reverse  = True)   
-    return scores[0]   
+        score = fit_tree(X_train,X_test,y_train,y_test, alpha)
+        if score>max_score:
+            max_score = score                  
+    return max_score  
 
 def fit_tree(X_train,X_test,y_train,y_test, alpha):
     "get the accuracy score of a specific tree with given ccp_alpha"
     if(alpha < 0):
         alpha = 0
-    dec_tree_a = DecisionTreeClassifier(random_state = rseed, ccp_alpha = alpha)
+    dec_tree_a = DecisionTreeClassifier(random_state = r_seed, ccp_alpha = alpha)
     dec_tree_a.fit(X_train,y_train)
     y_pred = dec_tree_a.predict(X_test)   
     return accuracy_score(y_test, y_pred)
@@ -95,15 +90,15 @@ class tests():
     """
     def __init__(self):
         #This is used for consistent train test splits for a given runtime for all models
-        #however, it has been replaced with a constant seed to produce simlair consistencey over runtimes
-        #rseed is used instead
+        #however, it has been replaced with a constant seed to produce similair consistency over runtimes
+        #r_seed is used instead
         self.seed = random.randint(0, 42) 
 
     def preprocess(self, i,X, y):
-        """Make the data ready for the model"""
-        #stratefy makes sure the same proportions of target values in the full dataset is preseved in the
-        #train and test data both the X_test and y_test are split 50/50
-        X_train,X_test,y_train,y_test = train_test_split(X,  y, test_size = 50, random_state = rseed, stratify=y)
+        """transform the data to be ready for a model"""
+        # stratify makes sure the same proportions of target values in the full dataset is preseved in the train and test data 
+        # both the y_train and y_test are split 50/50 between any given pair
+        X_train,X_test,y_train,y_test = train_test_split(X,  y, test_size = 50, random_state = r_seed, stratify=y)
 
         selector = SelectKBest(chi2, k=i)
         #only train data is used to fit the selectKbest selector...
@@ -115,7 +110,7 @@ class tests():
 
         return X_train,X_test,y_train,y_test
 
-    def test_features(self,i, model_id, X, y ):
+    def test_features(self,i, classifier_id, X, y ):
         """Train and test a model after preprocessing"""
         #https://blog.finxter.com/python-list-copy/
         #copy is atomic and rest of the variables are thread safe in the current namespace
@@ -126,34 +121,33 @@ class tests():
         X_train,X_test,y_train,y_test = self.preprocess(i,X, y)   
 
         #model selector
-        #the negation of the real accuracy is returned from test_features because the fminbound optmizer that uses this function...
+        #the negation of the real accuracy is returned from test_features because the fminbound optimizer that uses this function...
         #tries to find the minimum by default.
         #the optimizers output value can be negated again upon termination of the optimizer to give the real accuracy
-
-        if("dec_tree_model" == model_id):
+        if("dec_tree_model" == classifier_id):
             return -dec_tree_model(X_train,X_test,y_train,y_test)
-        if("log_reg_model" == model_id):
+        if("log_reg_model" == classifier_id):
             return -log_reg_model(X_train,X_test,y_train,y_test)
-        if("rand_forest_model" == model_id):
+        if("rand_forest_model" == classifier_id):
             return -rand_forest_model(X_train,X_test,y_train,y_test)      
-        if("naive_bays_model" == model_id):
+        if("naive_bays_model" == classifier_id):
             return -naive_bays_model(X_train,X_test,y_train,y_test)
         
 
 #main
 
-time_t = time.time()
+start_time = time.time()
 f = open('tf_matrix.csv', 'r', encoding="utf-8")
 data = pd.read_csv(f, header=0) 
 
-#pairs used for outputs
+#personality pairs used for outputs
 pairs  =  ["_I_E_","_N_S_", "_T_F_", "_J_P_"] 
 
-#features are all word/tokens that occur in at least one (from word bank in the transfrom.py programm)
+#features are all word/tokens that occur in at least one of the users posts (based on word bank in the transfrom.py programm)
 features = list(data.keys())
 features = features[:-5]
 
-#X is all the occurances of the words for each user the tf mtrix from tf_mtrix.csv (not the personality data)
+#X is the tf matrix from tf_mtrix.csv (not the personality data)
 X = data[features]
 X = X.values
 
@@ -163,22 +157,16 @@ X = csr_matrix(X)
 classification_tests = tests()
 
 
-#this is what it is:
-#a list of the accuracy scores for the best model of each model type (Decision Tree, Logistic Regession, Random Forest, Naive Bays) for every personalitiy pair
-#this list becomes a (4 personality pairs X 4 model types) = 16 model accuracies
+# best_in_class is a list of (lists/pairs) that includes four number of optimal features for each of the four classifers on the personailty pair type...
+# and four accuracy scores corresponding to these number of optimal features by index
 
+#the eventual shape of best_in_class is [4][2][4]
+#note: the "pair" (second dimension) corresponds to a personality pair (one of these ("_I_E_","_N_S_", "_T_F_", "_J_P_"))
 
-#this is how it is used:
-#this is used to multiply the accuracy scores of the same model type for each personailty pair to aproximate the comeplete personailty accuracy score
-#for a given model. This means there will be 4 accuracy scores (one for eahc model)
-#this is also used to find the absolute best model (take the best models from the Best in class for each personality pair)
-Best_in_class = []
+#best_in_class is used to theoreticly compute the accuracy of any single classifer type predcting the full personality (4 types correctly)
+#best_in_class is also used to theoreticlly compute the accuracy when the best classfier type is used to predict each personailty pair (4 types correctly)
+best_in_class = []
 
-#used to output to the results.csv file to observe outpus in a different way
-#note: may need to change later
-#LOOK
-#this is being replaced
-# out_df = pd.DataFrame()
 
 print("Training and testing with (39 X 16) = 624 users for each personality pair\n")
 
@@ -197,8 +185,8 @@ for item in pairs:
     print(item, "Classification:")
     while(i<6):           
         #optimiztion function fminbound finds the best value between the bounds of the number of features ((i)*gap_size+1 to (i+1)*gap_size+1)...
-        #that maximizes model perfromance
-        #multithreading is used here for small to medium improvements in runtime  
+        #that maximizes model perfrormance
+        #multithreading is used here for potential improvements in runtime  
         t1 = return_thread(group=None,target=fminbound,
                            kwargs={"func" : classification_tests.test_features,"x1" : (i)*gap_size+1, "x2" : (i+1)*gap_size+1,"args": ("dec_tree_model",X, y), "full_output" : True, "disp" :0})
         t2 = return_thread(group=None,target=fminbound,
@@ -217,11 +205,6 @@ for item in pairs:
         local_optima_for = t3.join()
         local_optima_bays = t4.join()   
         
-        # slower version of the above without the use of multithreading
-        # local_optima_1 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("dec_tree_model",X, y), full_output  = True, disp   =0)
-        # local_optima_2 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("log_reg_model",X, y), full_output  = True, disp   =0)
-        # local_optima_3 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("rand_forest_model",X, y), full_output  = True, disp   =0)
-        # local_optima_4 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("naive_bays_model",X, y), full_output  = True, disp   =0)
         
         #the negation is returned (see test_features function)
         local_optimas_dec.append((-local_optima_dec[1], int(local_optima_dec[0])))
@@ -242,7 +225,7 @@ for item in pairs:
         
         i += 1
 
-    #find the model with the most optimal number of features for each classfier for the the given pair
+    #find the model with the most optimal number of features for each classfier for the given pair
     #the optimal model has the highest accuracy for the classfier in question
     optimal_dec = max(local_optimas_dec, key = lambda x: x[0])
     optimal_reg = max(local_optimas_reg, key = lambda x: x[0])
@@ -259,59 +242,59 @@ for item in pairs:
     print("Naive Bays:", "Features:",optimal_bays[1],"Accuracy:",optimal_bays[0],"\n")
     
 
-    #Best_in_class is a list of pairs that include four accuracy scores for each of the four classifers on the personailty type...
-    #and four number of optimal features corresponding to these accuracy scores
-    #each accuracy score is the best accuracy score for the coresponding classifer for the given pair
-    #each of the optimal number of features is the inputs to the model that produces this accuracy score
-    Best_in_class.append([[optimal_dec[0],
-                          optimal_reg[0],
-                          optimal_for[0], 
-                          optimal_bays[0]],
-                          [optimal_dec[1],
-                          optimal_reg[1],
-                          optimal_for[1], 
-                          optimal_bays[1]]])
+    # best_in_class is a list of (lists/pairs) that includes four number of optimal features for each of the four classifers on the personailty pair type...
+    # and four accuracy scores corresponding to these number of optimal features by index
+
+    #the eventual shape of best_in_class is [4][2][4]
+    #note: the "pair" (second dimension) corresponds to a personality pair (one of these ("_I_E_","_N_S_", "_T_F_", "_J_P_"))
+
+    #best_in_class is used to theoreticly compute the accuracy of any single classifer type predcting the full personality (4 types correctly)
+    #best_in_class is also used to theoreticlly compute the accuracy when the best classfier type is used to predict each personailty pair (4 types correctly)
+    best_in_class.append([[optimal_dec[0], optimal_reg[0], optimal_for[0],optimal_bays[0]],
+                          [optimal_dec[1], optimal_reg[1], optimal_for[1], optimal_bays[1]]])
 
 
-#used for outputs
+#classifier types used for outputs
 classifiers = ["Decision Tree", "Logistic Regression", "Random Forest", "Naive Bays"]
 
-print("Complete Myers Briggs Prediction for individual Classifiers:")
+print("Complete Myers Briggs Prediction for individual Classifiers:\n")
 
 f = open("results.txt","w")
 
-#find the best models that uses the same classifer type for each personality pair and out put them
+#find the best models that uses the same classifer type for each personality pair and output results
 for i in range (0,4):
     pair_wise_features = ""
     model_accuracy =1
     j = 0
-    for pair in Best_in_class:
+    for pair in best_in_class:
         model_accuracy*= pair[0][i]
-        pair_wise_features+= "Pair: "+pairs[j]+" Features: "+str(pair[1][i])
+        pair_wise_features+= "Pair: "+pairs[j]+" Features: "+str(pair[1][i])+" "
         j+=1
         
     print("Classifier:", classifiers[i])
     print("Accuracy:",str(model_accuracy))
-    print(pair_wise_features)
+    print(pair_wise_features,"\n")
     f.write("Classifier: "+classifiers[i])
     f.write("\n")
     f.write("Accuracy: "+str(model_accuracy))
     f.write("\n")
     f.write(pair_wise_features)
-    f.write("\n")
+    f.write("\n\n")
 
 
 
 #find the absolute best model...
-best_classifer_and_number_features_per_personailty_pair = ""
+best_classifer_and_nof_features_per_personailty_pair = []
 best_model_accuracy  = 1
 
 i = 0
-for pair in Best_in_class:
-    acc_with_model_type = list(zip(pair[0], pair[1], classifiers))
-    acc_with_model_type = sorted(acc_with_model_type, key = (lambda x: x[0]) ,reverse = True)
-    best_model_accuracy*= acc_with_model_type[0][0]
-    best_classifer_and_number_features_per_personailty_pair+=("Pair: "+str(pairs[i])+ " Classifier: "+str(acc_with_model_type[0][2])+" Number of features: "+str(acc_with_model_type[0][1])+" ")
+for pair in best_in_class:
+    classifier_acc_nof_features = list(zip(classifiers, pair[0], pair[1]))
+    classifier_max_acc_nof_features = max(classifier_acc_nof_features, key = (lambda x: x[1]))
+    best_model_accuracy*= classifier_max_acc_nof_features[1]
+    best_classifer_and_nof_features_per_personailty_pair.append("Pair: "+str(pairs[i])+\
+                                                                " Classifier: " +str(classifier_max_acc_nof_features[0])+\
+                                                                " Number of features: "+str(classifier_max_acc_nof_features[2]))
     i+=1
 
 
@@ -320,17 +303,20 @@ f.write("Myers Briggs Prediction from the best of each Classifier:")
 f.write("\n")
 f.write("Best model accuracy: " + str(best_model_accuracy))
 f.write("\n")
-f.write(best_classifer_and_number_features_per_personailty_pair)
+for item in best_classifer_and_nof_features_per_personailty_pair:
+    f.write(item)
+    f.write("\n")
 f.close()
+
 print("Myers Briggs Prediction from the best of each Classifier:")
 print("Best model accuracy:", best_model_accuracy)
-print(best_classifer_and_number_features_per_personailty_pair)
+for item in best_classifer_and_nof_features_per_personailty_pair:
+    print(item)
   
-
 
 #computation time
 print("Done")
-print("Full compute time:",float((time.time() - time_t)/60), "Minutes")
+print("Full compute time:",float((time.time() - start_time)/60), "Minutes")
 
 
 
@@ -370,7 +356,7 @@ print("Full compute time:",float((time.time() - time_t)/60), "Minutes")
 
     # #Best of class is a list of pairs that include four accuracy scores for each of the classifers
     # #and optimal number of features for used for each of the four classifiers
-    # Best_in_class.append([[local_optimas_dec[0][0],
+    # best_in_class.append([[local_optimas_dec[0][0],
     #                       local_optimas_reg[0][0],
     #                       local_optimas_for[0][0], 
     #                       local_optimas_bays[0][0]],
@@ -378,3 +364,10 @@ print("Full compute time:",float((time.time() - time_t)/60), "Minutes")
     #                       local_optimas_reg[0][1],
     #                       local_optimas_for[0][1], 
     #                       local_optimas_bays[0][1]]])
+
+
+            # slower version of the above without the use of multithreading
+        # local_optima_1 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("dec_tree_model",X, y), full_output  = True, disp   =0)
+        # local_optima_2 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("log_reg_model",X, y), full_output  = True, disp   =0)
+        # local_optima_3 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("rand_forest_model",X, y), full_output  = True, disp   =0)
+        # local_optima_4 = fminbound(classification_tests.test_features,x1 = (i)*gap_size+1, x2 =(i+1)*gap_size+1 , args = ("naive_bays_model",X, y), full_output  = True, disp   =0)
